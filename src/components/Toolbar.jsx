1696d6940cfd629ca8efba0b3ee4f12bbaf40370
SergioGord <gsricco@yandex.ru>
2022-11-09 19:58:25 +0300

    feat: add circle, eraser,line

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import "../styles/toolbar.scss"
 import toolState from "../store/toolState";
-import canvas from "./Canvas";
 import canvasState from "../store/canvasState";
 import Brush from "../tools/Brush";
 import Rect from "../tools/Rect";
+import Circle from "../tools/Circle";
+import Eraser from "../tools/Eraser";
+import Line from "../tools/Line";
 
 const Toolbar = () => {
     return (
         <div className="toolbar">
             <button className="toolbar__btn brush" onClick={()=>toolState.setTool(new Brush(canvasState.canvas))}/>
             <button className="toolbar__btn rect" onClick={()=>toolState.setTool(new Rect(canvasState.canvas))}/>
-            <button className="toolbar__btn circle"/>
-            <button className="toolbar__btn eraser"/>
-            <button className="toolbar__btn line"/>
+            <button className="toolbar__btn circle" onClick={()=>toolState.setTool(new Circle(canvasState.canvas))}/>
+            <button className="toolbar__btn eraser" onClick={()=>toolState.setTool(new Eraser(canvasState.canvas))}/>
+            <button className="toolbar__btn line" onClick={()=>toolState.setTool(new Line(canvasState.canvas))}/>
             <input style={{marginLeft:10}} type="color"/>
             <button className="toolbar__btn undo"/>
             <button className="toolbar__btn redo"/>
